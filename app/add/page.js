146743c62eeaa099c282b1a77397ba1dd0f992db
Denis <godlin086@gmail.com>
2025-01-15 19:39:16 +0300

    added Link

--- app/add/page.js
+++ app/add/page.js
@@ -1,10 +1,11 @@
+import Link from "next/link"
 import AddItem from "../../components/AddItem"
 
 export default function Add() {
   return (
     <div className="font-[family-name:var(--font-geist-mono)] container mx-auto max-w-4xl">
       <header className="p-4 text-4xl">
-        checkroom <a className="ml-2 px-4 py-1 border-2 rounded-full inline-block cursor-pointer text-sm sm:text-xl align-middle" href="/">Home</a>
+        checkroom <Link className="ml-2 px-4 py-1 border-2 rounded-full inline-block cursor-pointer text-sm sm:text-xl align-middle" href="/">Home</Link>
       </header>
       <main className="px-4 lg:px-0">
         <AddItem />
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import Link from "next/link"
 import Items from "../components/Items"
 
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-mono)] container mx-auto max-w-4xl">
       <header className="p-4 text-4xl">
-        checkroom <a className="ml-2 px-4 py-1 border-2 rounded-full inline-block cursor-pointer text-sm sm:text-xl align-middle" href="/add">Add item</a>
+        checkroom <Link className="ml-2 px-4 py-1 border-2 rounded-full inline-block cursor-pointer text-sm sm:text-xl align-middle" href="/add">Add item</Link>
       </header>
       <main className="">
         <Items />
