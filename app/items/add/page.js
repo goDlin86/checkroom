8bedad1403d1c75e0d1dd07dcec8235b2eba4cf0
Denis <godlin086@gmail.com>
2025-01-21 19:51:29 +0300

    need auth for add

--- app/items/add/page.js
+++ app/items/add/page.js
@@ -1,6 +1,12 @@
 import AddItem from "../../../components/AddItem"
+import { auth } from "../../../lib/auth"
 
-export default function Add() {
+export default async function Add() {
+  let session = await auth()
+  let user = session?.user
+
+  if (!user) return <p className="my-4 text-center">Need to sign in</p>
+
   return (
     <main className="px-4 lg:px-0">
       <AddItem />
