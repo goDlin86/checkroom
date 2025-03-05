5e217b8e5081ee70f4d74f6e6df95d398023732e
Denis <godlin086@gmail.com>
2025-03-05 19:11:14 +0300

    changed hash function

--- app/login/page.js
+++ app/login/page.js
@@ -16,7 +16,7 @@ const LoginButton = ({ provider }) => {
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex justify-center items-start md:items-center p-8">
-      <div className="w-full max-w-sm">
+      <div className="w-full max-w-sm font-bold">
         <p className="text-2xl text-center">Login</p>
         {['mailru', 'github', 'google'].map((provider, i) => (
           <LoginButton key={i} provider={provider} />
